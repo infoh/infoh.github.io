be9c607d30fe6e5e39b90f60419ab300d4ffa051
infoh <101322163+infoh@users.noreply.github.com>
2023-04-20 21:19:52 +0800

    Create search.json.js

---
+++ search.json.js
@@ -0,0 +1,91 @@
+[
+  
+    {
+      "title"    : "The Wandering Earth and the Ugly American",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2023/04/10/wandering-earth-ugly-american.html"/*tpa=http://dangrover.com/blog/2023/04/10/wandering-earth-ugly-american.html*/,
+      "date"     : "2023-04-10 06:00:00 -0400"
+    } ,
+  
+    {
+      "title"    : "Shoggoth-as-a-Service may not survive the rise of AI script kiddies",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2023/03/24/ai-script-kiddies.html"/*tpa=http://dangrover.com/blog/2023/03/24/ai-script-kiddies.html*/,
+      "date"     : "2023-03-24 06:00:00 -0400"
+    } ,
+  
+    {
+      "title"    : "Simulation Games Might Be What The World Needs Now",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2022/09/24/sim-games.html"/*tpa=http://dangrover.com/blog/2022/09/24/sim-games.html*/,
+      "date"     : "2022-09-24 06:00:00 -0400"
+    } ,
+  
+    {
+      "title"    : "How Chinese Apps Handled Covid-19",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2020/04/05/covid-in-ui.html"/*tpa=http://dangrover.com/blog/2020/04/05/covid-in-ui.html*/,
+      "date"     : "2020-04-05 06:00:00 -0400"
+    } ,
+  
+    {
+      "title"    : "A Sanity Test For Large Groups of PMs",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2020/01/20/a-sanity-test-for-large-groups-of-pms.html"/*tpa=http://dangrover.com/blog/2020/01/20/a-sanity-test-for-large-groups-of-pms.html*/,
+      "date"     : "2020-01-20 05:57:40 -0500"
+    } ,
+  
+    {
+      "title"    : "Bots won&#39;t replace apps. Better apps will replace apps.",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2016/04/20/bots-wont-replace-apps.html"/*tpa=http://dangrover.com/blog/2016/04/20/bots-wont-replace-apps.html*/,
+      "date"     : "2016-04-20 06:57:40 -0400"
+    } ,
+  
+    {
+      "title"    : "More Chinese Mobile UI Trends",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2016/01/31/more-chinese-mobile-ui-trends.html"/*tpa=http://dangrover.com/blog/2016/01/31/more-chinese-mobile-ui-trends.html*/,
+      "date"     : "2016-01-31 05:00:40 -0500"
+    } ,
+  
+    {
+      "title"    : "Chinese Mobile App UI Trends",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "blog/2014/12/01/chinese-mobile-app-ui-trends.html"/*tpa=http://dangrover.com/blog/2014/12/01/chinese-mobile-app-ui-trends.html*/,
+      "date"     : "2014-12-01 05:57:40 -0500"
+    } ,
+  
+    {
+      "title"    : "Inequality &amp; Mass Transit in the Bay Area",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "2013/05/07/inequality-and-mass-transit-in-the-bay-area.html"/*tpa=http://dangrover.com/2013/05/07/inequality-and-mass-transit-in-the-bay-area.html*/,
+      "date"     : "2013-05-07 04:13:55 -0400"
+    } ,
+  
+    {
+      "title"    : "Etude in Apple Store Promo",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "2012/02/04/etude-in-apple-store-promo.html"/*tpa=http://dangrover.com/2012/02/04/etude-in-apple-store-promo.html*/,
+      "date"     : "2012-02-04 03:13:55 -0500"
+    } ,
+  
+    {
+      "title"    : "Etude Acquired by Steinway &amp; Sons",
+      "category" : "",
+      "tags"     : "",
+      "url"      : "2011/09/11/etude-acquired-by-steinway-sons.html"/*tpa=http://dangrover.com/2011/09/11/etude-acquired-by-steinway-sons.html*/,
+      "date"     : "2011-09-11 04:14:31 -0400"
+    } 
+  
+]
